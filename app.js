410bf25f25bacdec8db10c0336785d9615c6fe05
MEHMET RAŞİD GENCOSMANOĞLU <mehmet.gencosmanoglu@jcr.local>
2020-09-30 08:34:39 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/expressdb');
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function () {
+    console.log('Connected to MongoDB');
+});
+
+// Check for DB errors
+db.on('error', function (err) {
+    console.log(err);
+})
+
+// Init App
+const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home Route
+app.get('/', function (req, res) {
+    Article.find({}, function (err, articles) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', {
+                heading: 'This is h1',
+                articles: articles
+            });
+        }
+
+    });
+});
+
+// Add Route
+app.get('/articles/add', function (req, res) {
+    res.render('add_article', {
+        heading: 'JCR - ER'
+    });
+});
+
+// Start Server
+app.listen(3000, function () {
+    console.log('Server started on port 3000...');
+});
