ff25bcf088ea65aaa28e46c6e0fe74c742be8493
MEHMET RAŞİD GENCOSMANOĞLU <mehmet.gencosmanoglu@jcr.local>
2020-10-20 13:55:38 +0300

    add_article functionality

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 // Init App
 const app = express();
 
-mongoose.connect('mongodb://localhost/expressdb');
+mongoose.connect('mongodb://localhost/expressdb', { useNewUrlParser: true });
 let db = mongoose.connection;
 
 // Check connection
@@ -26,6 +26,11 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: false })); 
+//Used to parse JSON bodies
+app.use(express.json()); 
+
 // Home Route
 app.get('/', function (req, res) {
     Article.find({}, function (err, articles) {
@@ -40,12 +45,27 @@ app.get('/', function (req, res) {
 
     });
 });
-
 // Add Route
 app.get('/articles/add', function (req, res) {
     res.render('add_article', {
-        heading: 'JCR - ER'
+        title: 'JCR - ER Add Article'
     });
+});
+
+app.post('/articles/add', function(req,res){
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+    article.save(function(err){
+        if(err){
+            console.log(err);
+            return;
+        }else{
+            res.redirect('/')
+        }
+    });
+
 });
 
 // Start Server
